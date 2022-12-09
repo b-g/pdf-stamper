949b5ca847814305aebe9902b34da9141f147c6c
Benedikt Groß <bg@benedikt-gross.de>
2022-12-09 09:13:50 +0100

    Added defaults and args --x --y --text --page

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 
+const defaults = {
+  x: 297,
+  y: 40,
+  text: '',
+  page: -1
+};
+
 const minimist = require('minimist');
-const argv = minimist(process.argv.slice(2));
+const argv = minimist(process.argv.slice(2), { 'default': defaults });
 const fs = require('fs');
 const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 
@@ -15,35 +22,27 @@ const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
   }
 
   if (argv.in && argv.out && argv.signature) {
-    await stampSignature(argv.in, argv.out, argv.signature, argv.text);
+    await stampSignature(argv);
   }
 
 })();
 
-async function stampSignature(srcPath, outPath, signaturePath, text) {
-  const existingPdfBytes = await fs.promises.readFile(srcPath);
+async function stampSignature(argv) {
+  const existingPdfBytes = await fs.promises.readFile(argv.in);
   const pdfDoc = await PDFDocument.load(existingPdfBytes);
   const pages = pdfDoc.getPages();
-  const lastPage = pages[pages.length-1];
-  const { width, height } = lastPage.getSize();
+  const pageIndex = parseInt(argv.page);
+  const page = pageIndex === -1 ? pages[pages.length - 1] : pages[pageIndex];
+  const { width, height } = page.getSize();
 
   const blue = rgb(0.05, 0.2, 1.0);
-  const pos = { x: width / 2, y: 40 };
-
-  const signaturePdfBytes = await fs.promises.readFile(signaturePath);
-  const [signature] = await pdfDoc.embedPdf(signaturePdfBytes);
-  const signatureDims = signature.scale(1.1);
-  lastPage.drawPage(signature, {
-    ...signatureDims,
-    x: pos.x,
-    y: pos.y,
-  });
+  const pos = { x: argv.x, y: argv.y };
 
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
   const textSize = 12;
   // line break hack
-  const textParsed = text.replace('<br/>','\n');
-  lastPage.drawText(textParsed, {
+  const textParsed = argv.text.replace('<br/>','\n');
+  page.drawText(textParsed, {
     x: pos.x,
     y: pos.y - 14,
     size: textSize,
@@ -52,15 +51,24 @@ async function stampSignature(srcPath, outPath, signaturePath, text) {
     color: blue
   });
 
-  lastPage.drawLine({
+  page.drawLine({
     start: { x: pos.x, y: pos.y },
     end: { x: pos.x + 140, y: pos.y },
     thickness: 1.5,
     color: blue
   });
 
+  const signaturePdfBytes = await fs.promises.readFile(argv.signature);
+  const [signature] = await pdfDoc.embedPdf(signaturePdfBytes);
+  const signatureDims = signature.scale(0.85);
+  page.drawPage(signature, {
+    ...signatureDims,
+    x: pos.x,
+    y: pos.y,
+  });
+
   const pdfBytes = await pdfDoc.save();
-  await fs.promises.writeFile(outPath, pdfBytes);
+  await fs.promises.writeFile(argv.out, pdfBytes);
 }
 
 function help() {
